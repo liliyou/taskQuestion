b6c41409f43736a19e4ca7af7c35915f877a840d
LiLi <liliyoucomputer@gmail.com>
2015-12-28 21:14:32 +0800

    增加離開機制

--- OpApp/app/src/main/java/demo/android/com/opapp/MainActivity.java
+++ OpApp/app/src/main/java/demo/android/com/opapp/MainActivity.java
@@ -3,9 +3,12 @@ package demo.android.com.opapp;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private long exitTime = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -13,4 +16,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Log.e("MainActivity", "taskId=" + this.getTaskId());
     }
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && (event.getAction() == KeyEvent.ACTION_DOWN)) {
+
+            if ((System.currentTimeMillis() - exitTime) > 2000) {
+                Toast.makeText(this, "再按一次返回鍵退出 App", Toast.LENGTH_SHORT).show();
+                exitTime = System.currentTimeMillis();
+            } else {
+                System.exit(0);
+                finish();
+            }
+            return true;
+        }
+        return super.onKeyDown(keyCode, event);
+    }
+
 }
